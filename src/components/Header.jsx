435569a34fcb1dbd6569c83921cc405eb6398b30
bruno coelho <brunocoelho66@gmail.com>
2025-06-21 15:17:37 -0300

    feat(header): tornar navegação responsiva com menu hamburguer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,40 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Header() {
-    return (
-        <header className="bc-header">
-            <nav>
-                <ul>
-                    <li><Link to="/">Pokémons</Link></li>
-                    <li><Link to="/types">Tipos</Link></li>
-                    <li><Link to="/abilities">Habilidades</Link></li>
-                    <li><Link to="/items">Itens</Link></li>
-                    <li><Link to="/moves">Movimentos</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
+  return (
+    <header className="bc-header">
+      <nav className="bc-nav">
+        <Link to="/" className="bc-logo-container" onClick={closeMenu}>
+          <img
+            src="https://img.icons8.com/color/48/insignia-3.png"
+            alt="Logotipo Catch API"
+            width="48"
+            height="48"
+          />
+          <h1>CatchAPI</h1>
+        </Link>
+
+        <button className="bc-menu-toggle" onClick={toggleMenu} aria-label="Abrir ou fechar menu">
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </button>
+
+        <ul className={`bc-nav-links ${isOpen ? 'open' : ''}`}>
+          <li><Link to="/" onClick={closeMenu}>Pokémons</Link></li>
+          <li><Link to="/types" onClick={closeMenu}>Tipos</Link></li>
+          <li><Link to="/abilities" onClick={closeMenu}>Habilidades</Link></li>
+          <li><Link to="/items" onClick={closeMenu}>Itens</Link></li>
+          <li><Link to="/moves" onClick={closeMenu}>Movimentos</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
